42fd44d018e6cbc78a9fc3d7d6ef422feb039325
Daniel_C07 <daniel20070125@gmail.com>
2026-07-22 20:59:36 -0400

    Add files via upload

---
+++ TEMPc++.cpp
@@ -0,0 +1,28 @@
+//Daniel Chen
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    /*
+    ifstream fin("name.txt");
+    if (!fin)
+    {
+    cout << "File not found!" << endl;
+    return EXIT_FAILURE;
+    }
+    */
+
+
+    return EXIT_SUCCESS;
+}
+
+/*
+Output Pase
+*/
